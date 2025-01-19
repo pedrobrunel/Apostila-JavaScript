77113a325e0a209d3e4a9c873a7263306df596a3
Pedro Antônio <pedroantoniorba@gmail.com>
2025-01-19 00:28:01 -0300

    Atualizando CSS e metodosDeTexto

---
+++ 7 - Métodos de texto/metodosDeTexto.js
@@ -0,0 +1,5 @@
+const alteraMaiusculo = document.getElementById("textoMaiusculo")
+alteraMaiusculo.textContent = "Texto em Maíusculo".toLocaleUpperCase()
+
+const alteraMinusculo = document.getElementById("textoMinusculo")
+alteraMinusculo.textContent = "Texto em Minusculo".toLocaleLowerCase()
