d59bbacb3f3fbfa5f50159babc10c705c3cba205
Pedro Antônio <pedroantoniorba@gmail.com>
2025-01-19 00:51:08 -0300

    melhorando o css e atualizando o metodosDeTexto

--- 7 - Métodos de texto/metodosDeTexto.js
+++ 7 - Métodos de texto/metodosDeTexto.js
@@ -3,3 +3,11 @@ alteraMaiusculo.textContent = "Texto em Maíusculo".toLocaleUpperCase()
 
 const alteraMinusculo = document.getElementById("textoMinusculo")
 alteraMinusculo.textContent = "Texto em Minusculo".toLocaleLowerCase()
+
+let mensagem = "O comprimento dessa váriavel é?"
+const alteraH1Length = document.getElementById("tamanhoString")
+alteraH1Length.textContent = `Tamanho do texto: ${mensagem.length}`
+
+let numeros = 123456
+const alteraH1Numero = document.getElementById("tamanhoNumero")
+alteraH1Numero.textContent = `Tamanho do número: ${String(numeros).length}`
